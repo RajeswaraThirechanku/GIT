2697b57bd108d636904e50375840fcfa22f346a5
RajeswaraThirechanku <rajeswarant2@gmail.com>
2022-05-03 19:19:19 +0530

    Rajes program

---
+++ src/test/java/org/git/GIT.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class GIT {
+	private void Test1() {
+		System.out.println("1");
+	}
+	private void Test2() {
+		System.out.println("2");
+	}
+	private void Test3() {
+		System.out.println("3");
+	}
+	private void Test4() {
+		System.out.println("4");
+	}
+
+}
